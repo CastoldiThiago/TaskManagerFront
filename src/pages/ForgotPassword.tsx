d6e12774357cc8e317e0bc685fb0776815870abf
Thiago Castoldi <castoldithiago4@gmail.com>
2026-01-06 00:32:50 -0300

    fix endpoints for reset password

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -20,7 +20,7 @@ const ForgotPassword: React.FC = () => {
     setErrorMessage(null);
 
     try {
-      const response = await fetch(`${config.backendUrl}/api/auth/forgot-password`, {
+      const response = await fetch(`${config.backendUrl}/auth/forgot-password`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: data.email }),
